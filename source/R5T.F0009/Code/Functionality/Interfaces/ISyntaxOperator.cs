4ee3084986fc813848df16a387bce02df8590276
davidcoats <david@getsafetycone.com>
2022-06-09 12:26:10 -0700

    Interim changes.

---
+++ source/R5T.F0009/Code/Functionality/Interfaces/ISyntaxOperator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+using R5T.T0132;
+
+
+namespace R5T.F0009 /// <see cref="R5T.F0009.Documentation"/>
+{
+    [FunctionalityMarker]
+    public interface ISyntaxOperator : IFunctionalityMarker
+    {
+        public CompilationUnitSyntax AddNamespace(
+            CompilationUnitSyntax compilationUnit,
+            NamespaceDeclarationSyntax @namespace)
+        {
+            var output = compilationUnit.AddMembers(@namespace);
+            return output;
+        }
+
+        public CompilationUnitSyntax AddUsingDirective(
+            CompilationUnitSyntax compilationUnit,
+            UsingDirectiveSyntax usingDirective)
+        {
+            var output = compilationUnit.AddUsings(usingDirective);
+            return output;
+        }
+    }
+}
